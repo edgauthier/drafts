0c9997f46612573414e216dfb5ccdc72d5655306
Ed Gauthier <ed@egauthier.net>
2019-03-10 16:35:01 -0400

    Files and functions for working with React apps

---
+++ draftsReact.js
@@ -0,0 +1,65 @@
+// draftsReact
+
+// Babel for transpiling JSX
+require('babel.min.js');
+
+const parseJsxDraft = d => {
+  const 
+    opts = {presets: ['stage-2', 'react']},
+    jsxToJs = input => Babel.transform(input, opts).code;
+
+  return d ? jsxToJs(d.content) : '';
+};
+
+const taggedJsx = tag => {
+  const 
+    tags = ['jsx', tag],
+    ds = Draft.query('', 'all', tags),
+    combineJsx = R.pipe(
+      R.map(parseJsxDraft),
+      R.join('\n')
+    );
+  return combineJsx(ds);
+};
+
+const taggedCss = tag => {
+  const 
+    tags = ['css', tag],
+    ds = Draft.query('', 'all', tags),
+    combineCss = R.pipe(
+      R.map(d => d.content),
+      R.join('\n')
+    );
+  return combineCss(ds);
+};
+
+const validateReact = (data, code) => {
+  require('react.min.js');
+  require('react-dom.min.js');
+  let document = {
+    getElementById: () => ''
+  };
+  const js = R.concat(data, code);
+  eval(js);
+};
+
+const runReact = (tag, title, data) => {
+  const props = {
+    title,
+    data: dataToJS(data),
+    css: taggedCss(tag),
+    reactApp: taggedJsx(tag),
+    imports: R.join('\n', [
+      readLib('ramda.js'),
+      readLib('react.min.js'),
+      readLib('react-dom.min.js'),
+    ])
+  };
+  
+  const
+    t = '[[template|react_html.txt]]',
+    d = templateDraft(t, props),
+    a = Action.find('HTML View');
+    
+  app.queueAction(a,d);
+};
